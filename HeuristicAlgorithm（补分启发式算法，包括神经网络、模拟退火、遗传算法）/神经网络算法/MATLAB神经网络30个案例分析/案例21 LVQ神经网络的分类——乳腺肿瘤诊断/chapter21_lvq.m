38eed0850e16dee3bdc7862fb7ce2bad86ee713c
HuangChongqing <1756260160@qq.com>
2018-02-08 23:05:48 +0800

    LVQ神经网络-乳腺肿瘤诊断

--- HeuristicAlgorithm（补分启发式算法，包括神经网络、模拟退火、遗传算法）/神经网络算法/MATLAB神经网络30个案例分析/案例21 LVQ神经网络的分类——乳腺肿瘤诊断/chapter21_lvq.m
+++ HeuristicAlgorithm（补分启发式算法，包括神经网络、模拟退火、遗传算法）/神经网络算法/MATLAB神经网络30个案例分析/案例21 LVQ神经网络的分类——乳腺肿瘤诊断/chapter21_lvq.m
@@ -4,7 +4,7 @@
 % <table border="0" width="600px" id="table1">	<tr>		<td><b><font size="2">该案例作者申明：</font></b></td>	</tr>	<tr>		<td><span class="comment"><font size="2">1：本人长期驻扎在此<a target="_blank" href="http://www.ilovematlab.cn/forum-158-1.html"><font color="#0000FF">板块</font></a>里，对<a target="_blank" href="http://www.ilovematlab.cn/thread-49221-1-1.html"><font color="#0000FF">该案例</font></a>提问，做到有问必答。</font></span></td></tr><tr>	<td><span class="comment"><font size="2">2：此案例有配套的教学视频，配套的完整可运行Matlab程序。</font></span></td>	</tr>	<tr>		<td><span class="comment"><font size="2">		3：以下内容为该案例的部分内容（约占该案例完整内容的1/10）。</font></span></td>	</tr>		<tr>		<td><span class="comment"><font size="2">		4：此案例为原创案例，转载请注明出处（<a target="_blank" href="http://www.ilovematlab.cn/">Matlab中文论坛</a>，<a target="_blank" href="http://www.ilovematlab.cn/forum-158-1.html">《Matlab神经网络30个案例分析》</a>）。</font></span></td>	</tr>		<tr>		<td><span class="comment"><font size="2">		5：若此案例碰巧与您的研究有关联，我们欢迎您提意见，要求等，我们考虑后可以加在案例里。</font></span></td>	</tr>		<tr>		<td><span class="comment"><font size="2">		6：您看到的以下内容为初稿，书籍的实际内容可能有少许出入，以书籍实际发行内容为准。</font></span></td>	</tr><tr>		<td><span class="comment"><font size="2">		7：此书其他常见问题、预定方式等，<a target="_blank" href="http://www.ilovematlab.cn/thread-47939-1-1.html">请点击这里</a>。</font></span></td>	</tr></table>
 % </html>
 %
-web browser http://www.ilovematlab.cn/thread-61656-1-1.html
+% web browser http://www.ilovematlab.cn/thread-61656-1-1.html
 %% 清空环境变量
 clear all
 clc
@@ -60,7 +60,7 @@ disp(['良性乳腺肿瘤确诊：' num2str(number_B_sim)...
 disp(['恶性乳腺肿瘤确诊：' num2str(number_M_sim)...
       '  误诊：' num2str(number_M-number_M_sim)...
       '  确诊率p2=' num2str(number_M_sim/number_M*100) '%']);
-web browser http://www.ilovematlab.cn/thread-61656-1-1.html  
+% web browser http://www.ilovematlab.cn/thread-61656-1-1.html  
 %%
 % 
 % <html>
